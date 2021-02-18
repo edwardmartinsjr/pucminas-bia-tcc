1b8ef0828d0b20b2ed7d479c033f43becba1179b
Edward Martins Jr <edwardmartinsjr@gmail.com>
2021-02-18 19:48:34 +0100

    fix schema name

--- airflow-dag/product_category_name_translation_test.py
+++ airflow-dag/product_category_name_translation_test.py
@@ -8,7 +8,7 @@ import product_category_name_translation as task
 
 # START TESTS:
 class Test(unittest.TestCase):
-    db_name = 'temp_olist_db'
+    db_name = os.getenv('MYSQL_DB')
     table_name = 'product_category_name_translation'
     file_full_path = './airflow-dag/dummy_files/'+table_name+'.csv'
 
