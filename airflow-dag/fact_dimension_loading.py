0643d1a3efc107108813098ce25f79cb4acb2b46
Edward Martins Jr <edwardmartinsjr@gmail.com>
2021-03-13 20:33:29 +0100

    add fact loading

--- airflow-dag/fact_dimension_loading.py
+++ airflow-dag/fact_dimension_loading.py
@@ -106,49 +106,71 @@ def year_dim(ds, **kwargs):
     query_execute(query,'d_year')
     
 def sales_fact(ds, **kwargs):
+    # CREATE TEMP TABLES
     query = '''
     DROP TABLE IF EXISTS olist_db.temp_city;
     SET @rownr=0;
-    CREATE TEMPORARY TABLE olist_db.temp_city
+    CREATE TABLE olist_db.temp_city
     SELECT @rownr:=@rownr+1 AS city_id, state_id, customers.customer_city AS city, customers.customer_id FROM (SELECT DISTINCT olist_customers_dataset.customer_id, customer_city, customer_state FROM olist_db.olist_customers_dataset AS olist_customers_dataset
     INNER JOIN olist_db.olist_orders_dataset AS olist_orders_datase ON olist_orders_datase.customer_id = olist_customers_dataset.customer_id) AS customers
     INNER JOIN olist_db.d_state AS location_state ON location_state.state = customers.customer_state;
+    '''
+    query_execute(query,'temp_city')
 
+    query = '''
     DROP TABLE IF EXISTS olist_db.temp_payment;
     SET @rownr=0;
-    CREATE TEMPORARY TABLE olist_db.temp_payment
+    CREATE TABLE olist_db.temp_payment
     SELECT @rownr:=@rownr+1 AS payment_id, type_id, order_id, payment_sequential, payment_installments, payment_value FROM olist_db.olist_order_payments_dataset AS payments_dataset
     INNER JOIN olist_db.d_payment_type AS payment_type ON payment_type.payment_type = payments_dataset.payment_type;
+    '''
+    query_execute(query,'temp_payment')
 
+    query = '''
     DROP TABLE IF EXISTS olist_db.temp_review;
-    CREATE TEMPORARY TABLE olist_db.temp_review
+    CREATE TABLE olist_db.temp_review
     SELECT review_id, order_id, review_score FROM olist_db.olist_order_reviews_dataset;
+    '''
+    query_execute(query,'temp_review')
 
+    query = '''
     DROP TABLE IF EXISTS olist_db.temp_hour;
     SET @rownr=0;
-    CREATE TEMPORARY TABLE olist_db.temp_hour
+    CREATE TABLE olist_db.temp_hour
     SELECT @rownr:=@rownr+1 AS hour_id, order_id, HOUR(order_approved_at) AS `hour` FROM olist_db.olist_orders_dataset
     WHERE order_approved_at IS NOT NULL;
+    '''
+    query_execute(query,'temp_hour')
 
+    query = '''
     DROP TABLE IF EXISTS olist_db.temp_day;
     SET @rownr=0;
-    CREATE TEMPORARY TABLE olist_db.temp_day
+    CREATE TABLE olist_db.temp_day
     SELECT @rownr:=@rownr+1 AS day_id, order_id, DAY(order_approved_at) AS `day` FROM olist_db.olist_orders_dataset
     WHERE order_approved_at IS NOT NULL;
+    '''
+    query_execute(query,'temp_day')
 
+    query = '''
     DROP TABLE IF EXISTS olist_db.temp_month;
     SET @rownr=0;
-    CREATE TEMPORARY TABLE olist_db.temp_month
+    CREATE TABLE olist_db.temp_month
     SELECT @rownr:=@rownr+1 AS month_id, order_id, MONTH(order_approved_at) AS `month` FROM olist_db.olist_orders_dataset
     WHERE order_approved_at IS NOT NULL;
+    '''
+    query_execute(query,'temp_month')
 
+    query = '''
     DROP TABLE IF EXISTS olist_db.temp_year;
     SET @rownr=0;
-    CREATE TEMPORARY TABLE olist_db.temp_year
+    CREATE TABLE olist_db.temp_year
     SELECT @rownr:=@rownr+1 AS year_id, order_id, YEAR(order_approved_at) AS `year` FROM olist_db.olist_orders_dataset
     WHERE order_approved_at IS NOT NULL;
+    '''
+    query_execute(query,'temp_year')                        
 
-    -- FACT_SALES --
+    # LOAD FACT SALES
+    query = '''
     INSERT INTO olist_db.f_sales (order_id, product_id, city_id, payment_id, review_id, hour_id, day_id, month_id, year_id, price)
     (SELECT 
     orders_dataset.order_id
@@ -174,7 +196,19 @@ def sales_fact(ds, **kwargs):
     LEFT JOIN olist_db.temp_year AS temp_year ON temp_year.order_id = orders_dataset.order_id
     WHERE order_approved_at IS NOT NULL);
     '''
-    query_execute(query,'f_sales')        
+    query_execute(query,'f_sales')
+    
+    # DROP TEMP TABLES
+    query = '''
+    DROP TABLE IF EXISTS olist_db.temp_city;
+    DROP TABLE IF EXISTS olist_db.temp_payment;
+    DROP TABLE IF EXISTS olist_db.temp_review;
+    DROP TABLE IF EXISTS olist_db.temp_hour;
+    DROP TABLE IF EXISTS olist_db.temp_day;
+    DROP TABLE IF EXISTS olist_db.temp_month;
+    DROP TABLE IF EXISTS olist_db.temp_year;
+    '''
+    query_execute(query,'f_sales')    
 
 # Execute query        
 def query_execute(query, table_name):
